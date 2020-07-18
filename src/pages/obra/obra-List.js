c016c9930023947b04c3ff53bf946b2ecccf6f02
LBadalo <aluno19991@ipt.pt>
2020-07-18 22:09:39 +0100

    Update obra-List.js

--- src/pages/obra/obra-List.js
+++ src/pages/obra/obra-List.js
@@ -83,7 +83,7 @@ export default class ObraListPage extends React.Component {
                             <SearchFormComponent search={(text) => this.getList(text)} />
                         </Col>
                         <Col xs={6}>
-                            <div className="buttons-container" style={{ marginBottom: "5px", position: "fixed", marginTop: "5px", marginLeft: "40%" }}> {/*Se estiver nos favoritos, não permite adicionar obras*/}
+                            <div className="buttons-container" style={{ marginBottom: "5px", position: "absolute", marginTop: "5px" }}> {/*Se estiver nos favoritos, não permite adicionar obras*/}
                                 {user && user.role === 1 && (
                                     <Button
                                         variant="outline-primary"
